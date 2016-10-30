3164d81225d53419a864d2105e99dc0d12049acc
Carol Willing <carolcode@willingconsulting.com>
2016-10-30 12:58:51 -0700

    Edit example to be more Pythonic

--- examples/ex_7segment_clock.py
+++ examples/ex_7segment_clock.py
@@ -1,40 +1,45 @@
 #!/usr/bin/python
 
-import time
-import datetime
+"""Clock Example
+
+   This example creates a digital clock that displays the current time
+   on the 4 char, 7 segment display.
+"""
+from datetime import datetime
+from time import sleep
 
 from Adafruit_LED_Backpack import SevenSegment
 
-# ===========================================================================
-# Clock Example
-# ===========================================================================
-segment = SevenSegment.SevenSegment(address=0x70)
+
+# Create display instance on default I2C address (0x70) and bus number.
+display = SevenSegment.SevenSegment(address=0x70)
 
 # Initialize the display. Must be called once before using the display.
-segment.begin()
+display.begin()
 
-print "Press CTRL+Z to exit"
+# Output 'exit' instruction to console
+print("Press CTRL+Z to exit the example.")
 
-# Continually update the time on a 4 char, 7-segment display
-while(True):
-  now = datetime.datetime.now()
-  hour = now.hour
-  minute = now.minute
-  second = now.second
+# Update continually the time on a 4 char, 7-segment display
+while True:
+    now = datetime.now()
+    hour = now.hour
+    minute = now.minute
+    second = now.second
 
-  segment.clear()
-  # Set hours
-  segment.set_digit(0, int(hour / 10))     # Tens
-  segment.set_digit(1, hour % 10)          # Ones
-  # Set minutes
-  segment.set_digit(2, int(minute / 10))   # Tens
-  segment.set_digit(3, minute % 10)        # Ones
-  # Toggle colon
-  segment.set_colon(second % 2)              # Toggle colon at 1Hz
+    display.clear()
+    # Set hours
+    display.set_digit(0, int(hour / 10))     # Tens
+    display.set_digit(1, hour % 10)          # Ones
+    # Set minutes
+    display.set_digit(2, int(minute / 10))   # Tens
+    display.set_digit(3, minute % 10)        # Ones
+    # Toggle colon
+    display.set_colon(second % 2)            # Toggle colon at 1Hz
 
-  # Write the display buffer to the hardware.  This must be called to
-  # update the actual display LEDs.
-  segment.write_display()
+    # Write the display buffer to the actual hardware display LEDs.
+    display.write_display()
 
-  # Wait a quarter second (less than 1 second to prevent colon blinking getting$
-  time.sleep(0.25)
+    # Wait a quarter second (use less than 1 second to prevent colon blinking)
+    sleep(0.25)
+
